3a4e6d7c7fe5c7b077315e669e9e9b0c8c10bbd8
TinWu <yyuuix@gmail.com>
2017-08-04 19:20:46 +0800

    加專業

---
+++ OutWeb/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace OutWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            ViewBag.IsFirstPage = false;
+        }
+
+        // all 靜態
+        public ActionResult Index()
+        {
+            ViewBag.IsFirstPage = true;
+            return View();            
+        }
+
+        // 公司簡介
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        // 聯絡我們
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ OutWeb/Controllers/ManageController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OutWeb.Controllers
+{
+    public class ManageController : Controller
+    {
+        public ManageController()
+        {
+            ViewBag.IsFirstPage = false;
+        }
+
+        // GET: Manage
+        public ActionResult Index()        {
+            
+            return View("Login");
+        }
+
+        // 登入頁
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // 最新消息
+        public ActionResult NewsList()
+        {
+            return View();
+        }
+        public ActionResult NewsData()
+        {
+            return View();
+        }
+
+        // 活動剪影
+        // 分類
+        public ActionResult ActivityKind()
+        {
+            return View();
+        }
+        public ActionResult ActivityKindData()
+        {
+            return View();
+        }
+        // 相片
+        public ActionResult ActivityList()
+        {
+            return View();
+        }
+        public ActionResult ActivityData()
+        {
+            return View();
+        }
+
+        // 修改密碼
+        public ActionResult ChangePW()
+        {
+            return View();
+        }
+    }
+}
