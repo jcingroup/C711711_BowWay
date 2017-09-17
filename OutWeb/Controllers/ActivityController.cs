e9416a12b298ef77de553cadb3368dc2a4b3bef3
Gary Chang <gary661116@gmail.com>
2017-09-18 00:40:57 +0800

    前台最新消息 & 活動花絮更新

--- OutWeb/Controllers/ActivityController.cs
+++ OutWeb/Controllers/ActivityController.cs
@@ -1,9 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OutWeb.Service;
+using Lib.Service;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+/*Json.NET相關的命名空間*/
+using Newtonsoft.Json;
 
 namespace OutWeb.Controllers
 {
     public class ActivityController : Controller
     {
+        DBService DB = new DBService();
+
         public ActivityController()
         {
             ViewBag.IsFirstPage = false;
@@ -11,20 +27,45 @@ namespace OutWeb.Controllers
 
         public ActionResult Index()
         {
-            return View("Album");
+            return RedirectToAction("Album");
         }
 
         // 套程式-活動剪影
         // 相簿
         public ActionResult Album()
         {
+            //定義變數
+            DataTable d_album;
+
+            //抓取消息資料
+            d_album = DB.Activity_List("", "sort desc", "Y", "");
+            
+            //設定傳值
+            ViewData["d_album"] = d_album;
+
             return View();
         }
 
         // 圖片列表
-        public ActionResult Photo()
+        public ActionResult Photo(string act_id = "")
         {
-            return View();
+            //定義變數
+            DataTable d_album;
+            DataTable d_album_img;
+
+            d_album = DB.Activity_List(act_id);
+            d_album_img = DB.Img_List(act_id, "", "Activity");
+
+            if (d_album.Rows.Count > 0)
+            {
+                ViewData["d_album"] = d_album;
+                ViewData["d_album_img"] = d_album_img;
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("index");
+            }
         }
     }
 }
--- OutWeb/Controllers/NewsController.cs
+++ OutWeb/Controllers/NewsController.cs
@@ -1,9 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OutWeb.Service;
+using Lib.Service;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+/*Json.NET相關的命名空間*/
+using Newtonsoft.Json;
 
 namespace OutWeb.Controllers
 {
     public class NewsController : Controller
     {
+        DBService DB = new DBService();
+
         public NewsController()
         {
             ViewBag.IsFirstPage = false;
@@ -11,20 +27,43 @@ namespace OutWeb.Controllers
 
         public ActionResult Index()
         {
-            return View("List");
+            return RedirectToAction("List");
         }
 
         // 套程式-最新消息
         // 列表
         public ActionResult List()
         {
+            //定義變數
+            DataTable d_news;
+
+
+            //抓取消息資料
+            d_news = DB.News_List("", "n_date desc", "Y", "", "", "", "");
+
+            //設定傳值
+            ViewData["d_news"] = d_news;
+
             return View();
         }
 
         // 內容
-        public ActionResult Content()
+        public ActionResult NewsData(string n_id = "")
         {
-            return View();
+            //定義變數
+            DataTable d_news;
+
+            d_news = DB.News_List(n_id, "", "Y", "", "", "", "");
+
+            if (d_news.Rows.Count > 0)
+            {
+                ViewData["d_news"] = d_news;
+                return View("Content");
+            }
+            else
+            {
+                return RedirectToAction("index");
+            }
         }
     }
 }
