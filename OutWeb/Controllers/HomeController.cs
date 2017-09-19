cc47c826d9b3ebeef1881b517cae50e683c34e02
Gary Chang <gary661116@gmail.com>
2017-09-19 23:56:13 +0800

    首頁最新消息

--- OutWeb/Controllers/HomeController.cs
+++ OutWeb/Controllers/HomeController.cs
@@ -1,9 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OutWeb.Service;
+using Lib.Service;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+/*Json.NET相關的命名空間*/
+using Newtonsoft.Json;
 
 namespace OutWeb.Controllers
 {
     public class HomeController : Controller
     {
+        DBService DB = new DBService();
+
         public HomeController()
         {
             ViewBag.IsFirstPage = false;
@@ -13,6 +29,15 @@ namespace OutWeb.Controllers
         public ActionResult Index()
         {
             ViewBag.IsFirstPage = true;
+            //定義變數
+            DataTable d_news;
+
+
+            //抓取消息資料
+            d_news = DB.News_List("", "n_date desc", "Y", "", "", "", "Y");
+
+            //設定傳值
+            ViewData["d_news"] = d_news;
             return View();            
         }
 
